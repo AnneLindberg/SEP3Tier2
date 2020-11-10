a34b94fe283f669d4d9bca83262a8168def9277b
AnneLindberg <aans@live.dk>
2020-11-10 09:39:59 +0100

    deleted pages etc. For some reason pages where added, it's a console app so should not have pages. Small TODO's added

--- Tier2/Network/IBookSaleNetwork.cs
+++ Tier2/Network/IBookSaleNetwork.cs
@@ -9,4 +9,5 @@ namespace Tier2.Network
     }
 }
 
-//TODO:: is it a console app? Why does it have pages then?   
+//TODO:: maybe change the Models and add a dummy model with a hello string? instead of passing the BookSale object. Depends on what is on the other tiers
+//TODO:: check Tier1 localhost. Does it have to be the same? 
