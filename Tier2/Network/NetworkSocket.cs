7f073ad91b412d80162555704cccc153daf5dce6
AnneLindberg <aans@live.dk>
2020-11-10 10:12:56 +0100

    fixed the startup and small change in NetworkSocket

--- Tier2/Network/NetworkSocket.cs
+++ Tier2/Network/NetworkSocket.cs
@@ -13,7 +13,7 @@ namespace Tier2.Network
 
         public NetworkSocket()
         {
-            _tcpClient = new TcpClient("127.0.0.1", 5000);
+            _tcpClient = new TcpClient("127.0.0.1", 1236);
             stream = _tcpClient.GetStream();
         }
 
--- Tier2/Startup.cs
+++ Tier2/Startup.cs
@@ -32,23 +32,15 @@ namespace Tier2
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+           
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
         }
     }
 }
