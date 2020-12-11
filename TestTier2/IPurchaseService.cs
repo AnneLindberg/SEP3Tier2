6a415dd4a3263dcb3da4c06e4b10259b2e4be217
Crisiluluman <kawazu-maner@hotmail.com>
2020-12-11 10:42:12 +0100

    Tier2 Testing

---
+++ TestTier2/IPurchaseService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tier2.Models;
+
+namespace TestTier2
+{
+    public interface IPurchaseService
+    {
+        public Task<IList<PurchaseRequest>> GetPurchaseRequestAsync(string username);
+        public Task<IList<PurchaseRequest>> GetPurchaseRequestFromIdAsync(int id);
+        public Task<IList<PurchaseRequest>> CreatePurchaseRequestAsync(IList<PurchaseRequest> purchaseRequests);
+        public Task DeletePurchaseRequestAsync(int id);
+    }
+}
---
+++ TestTier2/PurchaseService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tier2.Data.Network;
+using Tier2.Models;
+
+namespace TestTier2
+{
+    public class PurchaseService : IPurchaseService
+    {
+        private readonly INetwork DBConn;
+
+        public PurchaseService() {
+            DBConn = new NetworkSocket();
+        }
+        
+        
+        public async Task<IList<PurchaseRequest>> GetPurchaseRequestAsync(string username) {
+            return await DBConn.GetPurchaseRequest(username);
+        }
+
+        public async Task<IList<PurchaseRequest>> GetPurchaseRequestFromIdAsync(int id) {
+            return await DBConn.GetPurchaseRequestFromId(id);
+        }
+
+        public async Task<IList<PurchaseRequest>> CreatePurchaseRequestAsync(IList<PurchaseRequest> purchaseRequests) {
+            DBConn.CreatePurchaseRequest(purchaseRequests);
+
+            return purchaseRequests;
+        }
+
+        public async Task DeletePurchaseRequestAsync(int id) {
+            
+            DBConn.DeletePurchaseRequest(id);
+        }
+    }
+}
